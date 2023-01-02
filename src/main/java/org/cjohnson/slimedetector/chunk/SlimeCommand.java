3d2ab8bad5d0bcd08e6d54b4f0bbfefc1c57fa3b
cjohnson <collin.g.johnson@gmail.com>
2023-01-02 12:33:46 -0500

    Organize imports for SlimeCommand

--- src/main/java/org/cjohnson/slimedetector/chunk/SlimeCommand.java
+++ src/main/java/org/cjohnson/slimedetector/chunk/SlimeCommand.java
@@ -1,12 +1,10 @@
 package org.cjohnson.slimedetector.chunk;
 
-import org.bukkit.Chunk;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.cjohnson.slimedetector.world.SlimeIndicator;
-import org.cjohnson.slimedetector.world.SlimeRadio;
 
 public class SlimeCommand implements CommandExecutor {
     private SlimeIndicator indicator;
